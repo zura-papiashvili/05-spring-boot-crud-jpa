d693423665922aa64add9021cac998f21b5e385c
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-04 17:35:50 -0500

    Update EmployeeRestController.java

--- src/main/java/com/springcourse/crudDemo/rest/EmployeeRestController.java
+++ src/main/java/com/springcourse/crudDemo/rest/EmployeeRestController.java
@@ -1,27 +1,29 @@
 package com.springcourse.crudDemo.rest;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcourse.crudDemo.dao.EmployeeDAO;
+import com.springcourse.crudDemo.entity.Employee;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    // quick and dirty: inject employee dao
-
     private EmployeeDAO employeeDAO;
 
+    // quick and dirty: inject employee dao (use constructor injection)
     public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
         employeeDAO = theEmployeeDAO;
     }
 
-    // expose "/employees" and return list of employees
+    // expose "/employees" and return a list of employees
     @GetMapping("/employees")
-    public String findAll() {
-        return employeeDAO.findAll().toString();
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
     }
 
 }
