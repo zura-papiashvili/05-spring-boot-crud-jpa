2c5d2af9d280c7f50a901479dcb66c3e55d18f44
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-05 11:06:32 -0500

    feat: spring boot rest

--- src/main/java/com/springcourse/crudDemo/dao/EmployeeRepository.java
+++ src/main/java/com/springcourse/crudDemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.springcourse.crudDemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springcourse.crudDemo.entity.Employee;
 
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
